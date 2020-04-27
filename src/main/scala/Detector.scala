5dd59a3701d780d22848ce351f9224f8d03add38
diegosolis08 <diego.solis@wundermobility.com>
2020-04-27 08:02:34 +0200

    Updated to use DS typed

--- src/main/scala/Detector.scala
+++ src/main/scala/Detector.scala
@@ -1,8 +1,15 @@
 package main
 
-import org.apache.spark.sql.{DataFrame, SaveMode, SparkSession}
+import org.apache.spark.sql.{Dataset, Encoders, SaveMode, SparkSession}
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.expressions.Window
+import org.apache.spark.sql.expressions.scalalang.typed
+
+case class Account(number: String, firstName: String, lastName: String)
+
+case class Transaction(id: Long, account: Account, date: java.sql.Date, amount: Double, description: String)
+
+case class TransactionForAverage(accountNumber: String, amount: Double, description: String, date: java.sql.Date)
 
 object Detector{
   def main(args: Array[String]) {
@@ -13,57 +20,61 @@ object Detector{
                             .enableHiveSupport()
                             .getOrCreate()
     import spark.implicits._
-    val financesDF = spark.read.json("Data/finances-small.json").cache()
+    val financesDS = spark.read.json("Data/finances-small.json")
+                          .withColumn("Date", to_date(unix_timestamp($"Date", "MM/dd/yyyy").cast("timestamp")))
+                          .as[Transaction]
+                          .cache()
 
     val accountNumberPrevious4WindowSpec = Window.partitionBy($"AccountNumber").orderBy($"Date").rowsBetween(-4, 0)
     val rollingAvgForPrevious4PerAccount = avg($"Amount").over(accountNumberPrevious4WindowSpec)
 
-    financesDF
+    financesDS
          .na.drop("all", Seq("ID", "Account", "Amount", "Description", "Date"))
-         .na.fill("Unknown", Seq("Description"))
+         .na.fill("Unknown", Seq("Description")).as[Transaction]
+        //  .filter(tx => (tx.amount != 0 || tx.description == "Unknown")) // This is the DS way to express the where, but it is not optimized by the Catalyst
          .where($"Amount" =!= 0 || $"Description" === "Unknown")
-         .selectExpr("Account.Number as AccountNumber", 
-                     "Amount", 
-                     "to_date(cast(unix_timestamp(Date, 'MM/dd/yyyy') AS TIMESTAMP)) AS Date", 
-                     "Description")
+         .select($"Account.Number".as("AccountNumber").as[String], 
+                 $"Amount".as[Double], 
+                 $"Date".as[java.sql.Date](Encoders.DATE), 
+                 $"Description".as[String])
          .withColumn("RollingAverage", rollingAvgForPrevious4PerAccount)
          .write.mode(SaveMode.Overwrite).parquet("Output/finances-small")
     
     // Output the records that are corrupt
-    if (financesDF.hasColumn("_corrupt_record")) {
-      financesDF.where($"_corrupt_record".isNotNull).select($"_corrupt_record")
+    if (financesDS.hasColumn("_corrupt_record")) {
+      financesDS.where($"_corrupt_record".isNotNull).select($"_corrupt_record")
                 .write.mode(SaveMode.Overwrite).text("Output/corrupt-finances")
     }
 
     // Query to get account full name and account number
-    financesDF
-          .select(concat($"Account.FirstName", lit(" "), $"Account.LastName").as("FullName"),
-                  $"Account.Number".as("AccountNumber"))
+    financesDS          
+          .map(tx => (s"${tx.account.firstName} ${tx.account.lastName}", tx.account.number))
           .distinct
+          .toDF("FullName", "AccountNumber")
           .coalesce(5)
           .write.mode(SaveMode.Overwrite).json("Output/finances-small-accounts")
     
     // Query to get account details
-    financesDF
-          .select($"Account.Number".as("AccountNumber"), 
-                  $"Amount", 
-                  $"Description", 
-                  to_date(unix_timestamp($"Date", "MM/dd/yyyy").cast("timestamp")).as("Date"))
-          .groupBy($"AccountNumber")
-          .agg(avg($"Amount").as("AverageTransaction"),
-               sum($"Amount").as("TotalTransactions"),
-               count($"Amount").as("NumberOfTransactions"),
-               max($"Amount").as("MaxTransaction"),
-               min($"Amount").as("MinTransaction"),
-               stddev($"Amount").as("StandardDeviationAmount"),
-               collect_set($"Description").as("UniqueTransactionDescriptions"))
+    financesDS
+          .select($"Account.Number".as("AccountNumber").as[String], 
+                  $"Amount".as[Double], 
+                  $"Description".as[String],
+                  $"Date".as[java.sql.Date](Encoders.DATE)).as[TransactionForAverage]
+          .groupByKey(_.accountNumber)
+          .agg(typed.avg[TransactionForAverage](_.amount).as("AverageTransaction").as[Double],
+               typed.sum[TransactionForAverage](_.amount)/*.as("TotalTransactions")*/,
+               typed.count[TransactionForAverage](_.amount)/*.as("NumberOfTransactions")*/,
+               max($"Amount").as("MaxTransaction").as[Double]/*, // This method has a limit of 4 parameters
+               min($"Amount").as("MinTransaction").as[Double],
+               stddev($"Amount").as("StandardDeviationAmount").as[Double],
+               collect_set($"Description").as("UniqueTransactionDescriptions")*/)
           .coalesce(5)
           .write.mode(SaveMode.Overwrite).json("Output/finances-small-account-details")
   }
 
   // Implicit class to add the hasColumn method available to DataFrame
-  implicit class DataFrameHelper(df: DataFrame) {
+  implicit class DataSetHelper[T](ds: Dataset[T]) {
     import scala.util.Try
-    def hasColumn(columnName: String) = Try(df(columnName)).isSuccess
+    def hasColumn(columnName: String) = Try(ds(columnName)).isSuccess
   }
 }
